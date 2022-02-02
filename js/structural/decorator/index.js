209833104ac45256afb8a91ccdac79dfb2c511b6
Yury Viahera <yuravegero@gmail.com>
2022-02-02 19:04:23 +0300

    Decorator pattern

--- js/structural/decorator/index.js
+++ js/structural/decorator/index.js
@@ -1,40 +1,42 @@
-//the base object
-class Cart {
-  constructor(name, sku, price) {
-    this.name = name;
-    this.sku = sku;
-    this.price = price;
-  }
-
-  getCart() {
-    return 'in your cart is a ' + this.name;
+class IChristmasTree {
+  decorate() {
   }
 }
 
-const cart = new Cart("clock", 2984, 15.99);
-console.log(cart.getCart());
-
-//extending with the decorator
-//the entry decorator
-class CartDecorator {
-  constructor(cart) {
-    this.cart = cart;
-  }
-
-  getCartItem() {
-    return this.cart.getCart();
+class ChristmasTree extends IChristmasTree {
+  decorate() {
+    return "Christmas tree";
   }
 }
 
-//the extension
-class ShippingDecorator extends CartDecorator {
-  getShipping() {
-    return 'shipping for '
-      + this.cart.name
-      + ' is $'
-      + this.cart.price * 0.25;
+class TreeDecorator extends ChristmasTree {
+  tree;
+
+  constructor(tree) {
+    super();
+    this.tree = tree;
+  }
+
+  decorate() {
+    return this.tree.decorate();
   }
 }
 
-const shipping = new ShippingDecorator(cart);
-console.log(shipping.getShipping());
+class BubbleLights extends TreeDecorator {
+
+  constructor(tree) {
+    super(tree);
+  }
+
+  decorate() {
+    return super.decorate() + this.decorateWithBubbleLights();
+  }
+
+  decorateWithBubbleLights() {
+    return " with Bubble Lights";
+  }
+}
+
+const tree = new BubbleLights(new ChristmasTree());
+
+console.log(tree.decorate())
